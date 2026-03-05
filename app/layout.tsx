13ff882fde5fee492c8b882b2bf5fa2af24a7752
Moffy5612 <moffy3318rami@gmail.com>
2026-03-05 18:33:39 +0900

    フッターの著作権表記を更新し、ゲーム画像の権利に関する説明を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
       <body>
         <main className="appMain">{children}</main>
         <footer style={{ padding: "16px", color: "#374151", fontSize: "12px", lineHeight: 1.6 }}>
-          <div>(C)mixi, Inc. All rights reserved.</div>
+          <div>(C)hototogisu2003 All rights reserved.</div>
           <div>
-            ※当サイト上で使用しているゲーム画像の著作権および商標権、その他知的財産権は、当該コンテンツの提供元に帰属します。
+            当サイト上で使用しているゲーム アセットの全ての権利は、mixi Inc.に帰属します。その他の知的財産権は、各権利所有者に帰属します。
           </div>
         </footer>
       </body>
